9cb75c1ca67d5c0d7c4ab79fb2e00805982caf4b
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2023-02-05 03:34:48 +0530

    terraform testings

---
+++ excercises/ec2.tf
@@ -0,0 +1,11 @@
+/*provider "aws" {
+  region = "ap-northeast-1"
+  access_key = "AKIAUDG26IV34MQYBLLV"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0bba69335379e17f8"
+  instance_type = "t2.micro"
+}
+*/
